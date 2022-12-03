22b61c660984308f0771f4038cd940817b1bfda6
Yadunand Prem <yadunut@gmail.com>
2022-12-04 01:56:57 +0800

    feat: day 2 and 3

--- advent-of-code-2022/day2/1.ts
+++ advent-of-code-2022/day2/1.ts
@@ -1,6 +1,36 @@
 import { readFileSync } from "fs";
 import { join } from "path";
 
+enum RPS {
+	Rock = 1,
+	Paper = 2,
+	Scissors = 3,
+}
+
+enum Result {
+	Draw = 3,
+	Win = 6,
+	Lose = 0,
+}
+
+const CONDITIONS = {
+	[RPS.Rock]: {
+		[RPS.Rock]: Result.Draw,
+		[RPS.Paper]: Result.Win,
+		[RPS.Scissors]: Result.Lose,
+	},
+	[RPS.Paper]: {
+		[RPS.Rock]: Result.Lose,
+		[RPS.Paper]: Result.Draw,
+		[RPS.Scissors]: Result.Win,
+	},
+	[RPS.Scissors]: {
+		[RPS.Rock]: Result.Win,
+		[RPS.Paper]: Result.Lose,
+		[RPS.Scissors]: Result.Draw,
+	},
+};
+
 async function main() {
 	const fileContents = readFileSync(join(__dirname, "./1.in"), {
 		encoding: "utf-8",
@@ -12,23 +42,22 @@ async function main() {
 
 function part1(file: string[]): number {
 	return file
-		.map((s) => s.split(" ").map((s) => nomalise(s)))
+		.map((s) => s.split(" ").map((s) => nomalise_rps(s)))
 		.map((rps) => calculateScore1(rps[0], rps[1]))
 		.reduce((a, b) => a + b);
 }
 
 function part2(file: string[]): number {
-	console.log(RPS[2]);
-	return 0;
+	return file
+		.map((s) => s.split(" "))
+		.map((rps) =>
+			calculateScore2(nomalise_rps(rps[0]), nomalise_result(rps[1])),
+		)
+		.reduce((a, b) => a + b);
+
 }
 
-enum RPS {
-	Rock = 1,
-	Paper = 2,
-	Scissors = 3,
-}
-
-function nomalise(a: string): RPS {
+function nomalise_rps(a: string): RPS {
 	switch (a) {
 		case "A":
 		case "X":
@@ -44,29 +73,25 @@ function nomalise(a: string): RPS {
 	}
 }
 
-function calculateScore2(other: RPS, current: RPS): number {
-	if (current === RPS.Rock) {
-		// lose
-		const result = RPS.Rock;
-		return 0 + result;
-	} else if (current === RPS.Paper) {
-		// draw
-		return 3 + other;
+function nomalise_result(a: string): Result {
+	if (a === "X") {
+		return Result.Lose;
+	} else if (a === "Y") {
+		return Result.Draw;
 	} else {
-		const result = RPS.Rock;
-		return 0 + result;
-		// win
+		return Result.Win;
 	}
 }
 
+function calculateScore2(other: RPS, result: Result): number {
+	const current = Object.keys(CONDITIONS[other]).find(
+		(key) => CONDITIONS[other][Number(key) as RPS] === result
+	);
+	return Number(current) + result;
+}
+
 function calculateScore1(other: RPS, current: RPS): number {
-	if (other === current) {
-		return other + 3;
-	}
-	if (current - other === 1 || current - other === -2) {
-		return current + 6;
-	}
-	return current;
+	return current + CONDITIONS[other][current];
 }
 
 main();
