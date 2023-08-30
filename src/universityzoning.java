1b50a5f37bc41f29f0fdccbd12a8dca46d96128e
Yadunand Prem <yadunand@yadunut.com>
2023-08-30 20:49:23 +0800

    feat: universityzoning

--- src/universityzoning.java
+++ src/universityzoning.java
@@ -1,74 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class universityzoning {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        sc.nextDouble(); // rows
-        sc.nextDouble(); // columns
-        int F = sc.nextInt(); // num faculties
-        int S = sc.nextInt(); // num students
-        int G = sc.nextInt(); // compliant faculties
+    public static void main(String[] args) throws IOException {
+        BufferedReader bi = new BufferedReader(new InputStreamReader(System.in));
+        int F, S, G;
+        {
+            String[] tokens = bi.readLine().split(" ");
+            F = Integer.parseInt(tokens[2]); // num faculties
+            S = Integer.parseInt(tokens[3]); // num students
+            G = Integer.parseInt(tokens[4]); // compliant faculties
+        }
 
-        int moves = 0;
+        long moves = 0;
 
         ArrayList<Faculty> faculties = new ArrayList<>();
 
+        // populate the faculties
         for (int i = 0; i < F; i++) {
-            int numCells = sc.nextInt();
+            String[] tokens = bi.readLine().split(" ");
+            int numCells = Integer.parseInt(tokens[0]);
             faculties.add(new Faculty());
             for (int j = 0; j < numCells; j++) {
-                faculties.get(i).cells.add(new Coord(sc.nextInt(), sc.nextInt()));
+                faculties.get(i).cells.add(new Coord(Long.parseLong(tokens[2 * j + 1]), Long.parseLong(tokens[2 * j + 2])));
             }
         }
 
+        // populate the students in each faculty
         for (int i = 0; i < S; i++) {
-            Coord coord = new Coord(sc.nextInt(), sc.nextInt());
-            int id = sc.nextInt();
-            int f = sc.nextInt() - 1;
-            // if student is already in valid cell, remove cell
-            if (faculties.get(f).cells.contains(coord)) {
-                faculties.get(f).cells.remove(coord);
-                faculties.get(f).minStudents--;
-                continue;
-            }
+            String[] tokens = bi.readLine().split(" ");
+            Coord coord = new Coord(Long.parseLong(tokens[0]), Long.parseLong(tokens[1]));
+            long id = Long.parseLong(tokens[2]);
+            int f = Integer.parseInt(tokens[3]) - 1;
             faculties.get(f).students.add(new Student(coord, id));
         }
 
-        ArrayList<Faculty> fToRemove = new ArrayList<>();
+        String[] tokens = bi.readLine().split(" ");
         for (int i = 0; i < F; i++) {
-            faculties.get(i).minStudents += sc.nextInt();
-            if (faculties.get(i).minStudents <= 0) {
-                fToRemove.add(faculties.get(i));
-                G--;
-            }
+            faculties.get(i).minStudents = Integer.parseInt(tokens[i]);
         }
-
-        if (G <= 0) {
-            System.out.println(moves);
-            return;
-        }
-        fToRemove.forEach(faculties::remove);
 
         faculties.forEach(f -> {
-            f.students.sort(Comparator.comparingInt(a -> a.id));
-            f.cells.sort(Comparator.comparingInt(a -> a.x));
+            f.students.sort(Comparator.comparingLong(a -> a.id));
+            f.cells.sort(Comparator.comparingLong((Coord a) -> a.row).thenComparingLong(a -> a.col)); // sort the cells based on row, so that the highest cells (cells closest to 0, would be at the top)
+            // calculate the distances for each student[i] and cell[i]
             for (int i = 0; i < f.students.size(); i++) {
                 Student s = f.students.get(i);
                 Coord c = f.cells.get(i);
-                f.distances.add(Math.abs(s.coord.x - c.x) + Math.abs(s.coord.y - c.y));
+                f.distances.add(Math.abs(s.coord.row - c.row) + Math.abs(s.coord.col - c.col));
             }
-            f.distances.sort(Comparator.comparingInt(Number::intValue));
+            // after sorting, the i-th distance would be the i-th shortest path for the student
+            f.distances.sort(Comparator.comparingLong(Number::longValue));
+            // compute the minimum distance for this faculty to be compliant
             for (int i = 0; i < f.minStudents; i++) {
-                f.minTotalDistance += f.distances.get(i).intValue();
+                f.minTotalDistance += f.distances.get(i).longValue();
             }
         });
 
-        faculties.sort(Comparator.comparingInt(f -> f.minTotalDistance));
+        // sort the faculties on minimum distance needed
+        faculties.sort(Comparator.comparingLong(f -> f.minTotalDistance));
         // now I need to add up the minimum faculties needed
-        for (; G > 0; G--) {
-            for (Faculty f : faculties) {
-                moves += f.minTotalDistance;
-            }
+        for (int i = 0; i < G; i++) {
+            moves += faculties.get(i).minTotalDistance;
         }
         System.out.println(moves);
     }
@@ -79,44 +74,26 @@ class Faculty {
     public ArrayList<Student> students = new ArrayList<>();
     public ArrayList<Number> distances = new ArrayList<>();
     public int minStudents = 0;
-
-    public int minTotalDistance = 0;
-
-    @Override
-    public String toString() {
-        return "Faculty{" +
-                "cells=" + cells +
-                ", students=" + students +
-                ", minStudents=" + minStudents +
-                '}';
-    }
+    public long minTotalDistance = 0;
 }
 
 class Student {
     public final Coord coord;
-    public final int id;
+    public final long id;
 
-    Student(Coord coord, int id) {
+    Student(Coord coord, long id) {
         this.coord = coord;
         this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "coord=" + coord +
-                ", id=" + id +
-                '}';
     }
 }
 
 class Coord {
-    final int x;
-    final int y;
+    final long row;
+    final long col;
 
-    Coord(int x, int y) {
-        this.x = x;
-        this.y = y;
+    Coord(long x, long y) {
+        this.row = x;
+        this.col = y;
     }
 
     @Override
@@ -124,19 +101,11 @@ class Coord {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Coord coord = (Coord) o;
-        return x == coord.x && y == coord.y;
+        return row == coord.row && col == coord.col;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(x, y);
-    }
-
-    @Override
-    public String toString() {
-        return "Coord{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
+        return Objects.hash(row, col);
     }
 }
