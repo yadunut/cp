98e5290e23526223e76a04ec0a1b3b902660641e
Yadunand Prem <yadunand@yadunut.com>
2023-11-01 11:41:16 +0800

    feat: daily faster

--- src/leetcode/L501.java
+++ src/leetcode/L501.java
@@ -1,18 +1,35 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 //501. Find Mode in Binary Search Tree
 public class L501 {
     HashMap<Integer, Integer> map = new HashMap<>();
-    int maxFreq = 0;
+    int currVal;
+    int currCount = 0;
+    int maxCount = 0;
+    List<Integer> result = new ArrayList<>();
+
     public int[] findMode(TreeNode root) {
-        if (root == null) return new int[]{};
-        map.compute(root.val, (val, freq) -> freq != null ? freq+1 : 1);
-        maxFreq = Math.max(maxFreq, map.get(root.val));
-        findMode(root.left);
-        findMode(root.right);
-        return map.entrySet().stream().filter((v) -> v.getValue() == maxFreq).mapToInt(Map.Entry::getKey).toArray();
+        inOrder(root);
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+
+    public void inOrder(TreeNode root) {
+        if (root == null) return;
+        inOrder(root.left);
+        currCount = currVal == root.val ? currCount + 1 : 1;
+        if (currCount == maxCount) {
+            result.add(root.val);
+        } else if (currCount > maxCount) {
+            maxCount = currCount;
+            result.clear();
+            result.add(root.val);
+        }
+        currVal = root.val;
+        inOrder(root.right);
     }
 }
